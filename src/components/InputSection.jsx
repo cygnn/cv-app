95d6593f44efe0853b83fd0cf165e289e0bec803
Clyde <113117498+cygnn@users.noreply.github.com>
2024-10-06 14:39:34 +0800

    some minor changes

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -4,10 +4,14 @@ import GenInfo from "./GenInfo";
 import WorkExp from "./WorkExp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
 
 export default function InputSection({genInfoData, setGenInfoData, educExpData, setEducExpData, workExpData, setWorkExpData}){
     const [educComponent, setEducComponent] = useState([])
     const [workComponent, setWorkComponent] = useState([])
+    const [toggleDropdown, setToggleDropdown] = useState(false)
+    const [toggleDropdownEduc, setToggleDropdownEduc] = useState(false)
 
     const handleAdd = () => {
         setEducComponent([...educComponent, {id: educComponent.length}])
@@ -15,6 +19,14 @@ export default function InputSection({genInfoData, setGenInfoData, educExpData, 
     
     const handleExp = () => {
         setWorkComponent([...workComponent, {id: workComponent.length}])
+    }
+
+    const handleDropDown = () => {
+        setToggleDropdown(prevState => !prevState);
+    };
+
+    const handleDropDownEduc = () => {
+        setToggleDropdownEduc(!toggleDropdownEduc)
     }
 
     return (
@@ -26,32 +38,45 @@ export default function InputSection({genInfoData, setGenInfoData, educExpData, 
                     setGenInfoData = {setGenInfoData}
                 />
             </div>
-            <div className="work-info">
-                <h2 className="subtitle input">Work Experience</h2>
-                {workComponent.map((_) => (
-                    <WorkExp 
-                        key={_.id}
-                        index={_.id}
-                        workExpData = {workExpData}
-                        setWorkExpData = {setWorkExpData}
-                        workComponent = {workComponent}
-                        setWorkComponent = {setWorkComponent}
-                    />
-                ))}
-                <button onClick={handleExp}><FontAwesomeIcon icon={faPlus} /> Experience</button>
+            <div className={`work-info ${toggleDropdown ? 'active' : 'inactive'}`}>
+                <div className="header">
+                    <h2 className="subtitle input">Work Experience</h2>
+                    <button className='dropdown' onClick={handleDropDown}>{!toggleDropdown ? <FontAwesomeIcon icon={faCaretDown} /> : <FontAwesomeIcon icon={faCaretUp} />}</button>
+                </div>
+                <div className="body">
+                    {workComponent.map((_) => (
+                        <WorkExp 
+                            key={_.id}
+                            index={_.id}
+                            workExpData={workExpData}
+                            setWorkExpData={setWorkExpData}
+                            workComponent={workComponent}
+                            setWorkComponent={setWorkComponent}
+                        />
+                    ))}
+                    <button onClick={handleExp}>
+                        <FontAwesomeIcon icon={faPlus} /> Experience
+                    </button>
+                </div>
             </div>
-            <div className="educ-info">
-                {educComponent.map((_) => (
-                    <EducExp 
-                        key={_.id}
-                        index={_.id}
-                        educExpData = {educExpData}
-                        setEducExpData={setEducExpData}
-                        educComponent={educComponent}
-                        setEducComponent={setEducComponent}
-                    />
-                ))}
+            <div className={`educ-info ${toggleDropdownEduc ? 'active': 'inactive'}`}>
+                <div className="header">
+                    <h2 className="subtitle input">Education</h2>
+                    <button className="dropdown" onClick={handleDropDownEduc}>{!toggleDropdownEduc ? <FontAwesomeIcon icon={faCaretDown} /> : <FontAwesomeIcon icon={faCaretUp} />}</button>
+                </div>
+                <div className="body">
+                    {educComponent.map((_) => (
+                        <EducExp 
+                            key={_.id}
+                            index={_.id}
+                            educExpData = {educExpData}
+                            setEducExpData={setEducExpData}
+                            educComponent={educComponent}
+                            setEducComponent={setEducComponent}
+                        />
+                    ))}
                 <button onClick={handleAdd}><FontAwesomeIcon icon={faPlus} /> Education</button>
+                </div>
             </div>
         </div>
     )
